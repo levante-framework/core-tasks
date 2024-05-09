f669ba17bdf1b9fb89dc8d376603d537aa2b2c96
Adrien Nabonne <adrien@nabonne.fr>
2024-05-09 15:44:04 -0700

    Extra audio: remove unused setupReplayAudio

--- task-launcher/src/tasks/shared/helpers/replayAudio.js
+++ task-launcher/src/tasks/shared/helpers/replayAudio.js
@@ -1,43 +1,4 @@
-import { jsPsych } from "../../taskSetup";
-import { mediaAssets } from "../../..";
-import { camelize } from "@bdelab/roar-utils";
 import jsPsychAudioMultiResponse from "@jspsych-contrib/plugin-audio-multi-response";
-import { replayButtonDivId } from "./components";
-
-export function setupReplayAudio(audioSource, audioFile) {
-    // Hardcoded since it uses the replayButtonDiv comopnent
-    const replayBtn = document.getElementById(replayButtonDivId);
-
-    if (replayBtn) {
-      // TODO: this only stops the Replay button from being used if it was already used
-        let isAudioPlaying = false;  
-      
-        async function replayAudio() {  
-            if (isAudioPlaying) return
-
-            const jsPsychAudioCtx = jsPsych.pluginAPI.audioContext();
-
-            isAudioPlaying = true;
-
-            // Returns a promise of the AudioBuffer of the preloaded file path.
-            const audioBuffer = await jsPsych
-                .pluginAPI
-                .getAudioBuffer(mediaAssets.audio[camelize(audioFile)] ||
-                                mediaAssets.audio.nullAudio,);
-
-            audioSource = jsPsychAudioCtx.createBufferSource();
-            audioSource.buffer = audioBuffer;
-            audioSource.connect(jsPsychAudioCtx.destination);
-            audioSource.start(0);
-
-            audioSource.onended = () => {
-                isAudioPlaying = false;
-            };
-        }
-
-        replayBtn.addEventListener('click', replayAudio);
-    }
-}
 
 /**
  * Overrides some of the properties of a jsPsychAudioMultiResponse object to allow for replaying audio.
