89c475eeb88963a5ed5aea472dfd07eb1746eff6
George Kachergis <george.kachergis@gmail.com>
2024-06-20 15:01:36 -0700

    finish v1 introduction task

--- task-launcher/src/tasks/taskConfig.js
+++ task-launcher/src/tasks/taskConfig.js
@@ -83,7 +83,7 @@ export default {
   },
   intro: {
     setConfig: setSharedConfig,
-    getCorpus: null, // no corpus needed
+    getCorpus: getCorpus,
     getTranslations: getTranslations,
     buildTaskTimeline: introTimeline,
     variants: {},
