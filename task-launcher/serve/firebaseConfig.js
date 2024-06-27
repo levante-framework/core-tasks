6e13eedcf5438eea565e9b9b1d8bb8fddc83bf39
Zio-4 <philipz4848@gmail.com>
2024-06-27 12:07:37 -0700

    Adding production config

--- task-launcher/serve/firebaseConfig.js
+++ task-launcher/serve/firebaseConfig.js
@@ -11,12 +11,12 @@ const devFirebaseConfig = {
 };
 
 const productionFirebaseConfig = {
-  apiKey: 'AIzaSyC9IoJBEyN-BxHobeoMQRuEu0CtyQDOg8k',
-  authDomain: 'hs-levante-assessment-dev.firebaseapp.com',
-  projectId: 'hs-levante-assessment-dev',
-  storageBucket: 'hs-levante-assessment-dev.appspot.com',
-  messagingSenderId: '46792247600',
-  appId: '1:46792247600:web:ea20e1fe94e0541dd5a0f5',
+  apiKey: "AIzaSyCdAa6P5Ot8jjnKAi_FNLDfvWP_rDqeQYg",
+  authDomain: "hs-levante-assessment-prod.firebaseapp.com",
+  projectId: "hs-levante-assessment-prod",
+  storageBucket: "hs-levante-assessment-prod.appspot.com",
+  messagingSenderId: "928482088295",
+  appId: "1:928482088295:web:1cab64d5dccb2d19ae8bc2"
 };
 
 export const firebaseConfig = ENV === 'production' ? productionFirebaseConfig : devFirebaseConfig;
