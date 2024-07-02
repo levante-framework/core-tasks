52bc911162fc0145316c1ac4d4020c976c9538e1
zwatson2001 <zwatson@vassar.edu>
2024-07-02 14:21:46 -0400

    add replay button to trials and add audio to feedback screen

--- task-launcher/src/tasks/shared/trials/feedback.js
+++ task-launcher/src/tasks/shared/trials/feedback.js
@@ -1,8 +1,10 @@
 import jsPsychHTMLMultiResponse from '@jspsych-contrib/plugin-html-multi-response';
+import jsPsychAudioMultiResponse from '@jspsych-contrib/plugin-audio-multi-response'
 import { mediaAssets } from '../../..';
 import { taskStore } from '../helpers';
 
 // isPractice parameter is for tasks that don't have a corpus (e.g. memory game)
+/*
 export const feedback = (isPractice = false) => {
     return {
         timeline: [ 
@@ -30,6 +32,52 @@ export const feedback = (isPractice = false) => {
                 },
                 button_choices: [`Continue`],
                 keyboard_choices: 'ALL_KEYS',
+                button_html: () => {
+                    const t = taskStore().translations;
+                    return (`<button id="continue-btn">${t.continueButtonText}</button>`)
+                }
+            } 
+        ],
+        conditional_function: () => {
+            return taskStore().nextStimulus?.notes === 'practice' || taskStore().nextStimulus?.trialType === 'practice' || isPractice
+        },
+    }
+}
+*/
+export const feedback = (isPractice = false) => {
+    return {
+        timeline: [
+            {
+                type: jsPsychAudioMultiResponse,
+                stimulus: () => {
+                    const isCorrect = taskStore().isCorrect;
+                    return (
+                        isCorrect ? mediaAssets.audio.feedbackCorrect : mediaAssets.audio.memoryGameForwardPrompt
+                    )
+                },
+                prompt: () => {
+                    const t = taskStore().translations;
+                    const isCorrect = taskStore().isCorrect;
+                    return (
+                        `<div id='stimulus-container'>
+                            <div id='prompt-container-text'>
+                                <p id='prompt'>${isCorrect ? t.feedbackCorrect : t.heartsAndFlowersTryAgain}</p>
+                            </div>
+                    
+                            <img id='instruction-graphic' src=${isCorrect ? mediaAssets.images['smilingFace@2x'] : 
+                                    mediaAssets.images['sadFace@2x']} alt='Instruction graphic'/>
+
+                            ${isCorrect ? '' :
+                             `<div id='prompt-container-text'>
+                                <footer id='prompt'>${t.memoryGameForwardPrompt}</footer>
+                              </div>`
+                            }
+                        </div>`
+                    )  
+                },
+                button_choices: ['Continue'], 
+                keyboard_choices: 'NO_KEYS', 
+                prompt_above_buttons: true, 
                 button_html: () => {
                     const t = taskStore().translations;
                     return (`<button id="continue-btn">${t.continueButtonText}</button>`)
