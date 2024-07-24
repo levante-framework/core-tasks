85993d8fe5ec7cfc9d86a1711bee139ab2837990
Aritra <aritra@skillprint.co>
2024-07-24 16:11:17 +0530

    fixing the trial loops

--- task-launcher/src/tasks/hearts-and-flowers/timeline.js
+++ task-launcher/src/tasks/hearts-and-flowers/timeline.js
@@ -183,11 +183,17 @@ function getHeartOrFlowerInstructionsSection(adminConfig, stimulusType) {
   // Instruction practice trials do not advance until user gets it right
   subtimeline.push({
     timeline: [instructionPractice1, instructionPracticeFeedback],
-    loop_function: (data) => taskStore().correct === false,
+    loop_function: (data) => {
+      console.log('mark://', 'In loop', taskStore().correct === false, taskStore().correct, taskStore().isCorrect);
+      return taskStore().isCorrect === false;
+    }
   });
   subtimeline.push({
     timeline: [instructionPractice2, instructionPracticeFeedback],
-    loop_function: (data) => taskStore().correct === false,
+    loop_function: (data) => {
+      console.log('mark://', 'In loop', taskStore().correct === false);
+      return taskStore().isCorrect === false;
+    }
   });
 
   return subtimeline;
@@ -307,11 +313,11 @@ function getMixedInstructionsSection(adminConfig) {
   // Instruction practice trials do not advance until user gets it right
   subtimeline.push({
     timeline: [instructionPractice1, instructionPracticeFeedback],
-    loop_function: (data) => taskStore().correct === false,
+    loop_function: (data) => taskStore().isCorrect === false,
   });
   subtimeline.push({
     timeline: [instructionPractice2, instructionPracticeFeedback],
-    loop_function: (data) => taskStore().correct === false,
+    loop_function: (data) => taskStore().isCorrect === false,
   });
 
   return subtimeline;
