a4ec9e0ffe4db2922ea43b5df745553683182a76
zwatson2001 <zwatson@vassar.edu>
2024-07-31 15:37:37 -0400

    remove comment in feedback.js

--- task-launcher/src/tasks/shared/trials/feedback.js
+++ task-launcher/src/tasks/shared/trials/feedback.js
@@ -4,69 +4,6 @@ import { taskStore } from '../helpers';
 import { camelize } from '@bdelab/roar-utils';
 
 // isPractice parameter is for tasks that don't have a corpus (e.g. memory game)
-/*
-export const feedback = (isPractice = false) => {
-    return {
-        timeline: [ 
-            {
-                type: jsPsychAudioMultiResponse,
-                stimulus: () => {
-                    const isCorrect = taskStore().isCorrect;
-                    let promptText
-
-                    if (taskStore().task === 'memory-game'){
-                        promptText = `memoryGameInput`; 
-                    } else {
-                        promptText = camelize(taskStore().nextStimulus.audioFile); 
-                    }
-
-                    return (
-                        isCorrect ? mediaAssets.audio.feedbackCorrect : mediaAssets.audio[promptText]
-                    )
-                },
-                prompt: () => {
-                    const t = taskStore().translations;
-                    const isCorrect = taskStore().isCorrect;
-                    let promptText
-
-                    if (taskStore().task === 'memory-game'){
-                        promptText = `memoryGameInput`; 
-                    } else {
-                        promptText = camelize(taskStore().nextStimulus.audioFile); 
-                    }
-
-                    return (
-                        `<div id='stimulus-container'>
-                            <div id='prompt-container-text'>
-                                <p id='prompt'>${isCorrect ? t.feedbackCorrect : t.heartsAndFlowersTryAgain}</p>
-                            </div>
-                    
-                            <img id='instruction-graphic' src=${isCorrect ? mediaAssets.images['smilingFace@2x'] : 
-                                    mediaAssets.images['sadFace@2x']} alt='Instruction graphic'/>
-
-                            ${isCorrect ? '' :
-                             `<div id='prompt-container-text'>
-                                <footer id='prompt'>${t[promptText]}</footer>
-                              </div>`
-                            }
-                        </div>`
-                    )  
-                },
-                button_choices: [`Continue`],
-                keyboard_choices: 'NO_KEYS',
-                prompt_above_buttons: true,
-                button_html: () => {
-                    const t = taskStore().translations;
-                    return (`<button class="primary">${t.continueButtonText}</button>`)
-                }
-            } 
-        ],
-        conditional_function: () => {
-            return taskStore().nextStimulus?.notes === 'practice' || taskStore().nextStimulus?.trialType === 'practice' || isPractice
-        },
-    }
-}
-*/
 export const feedback = (isPractice = false) => {
     return {
         timeline: [
