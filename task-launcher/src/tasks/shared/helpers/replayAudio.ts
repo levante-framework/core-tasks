01fdd1b5d2e04677965561c0c9dfd54a9fa065f8
Aritra <aritra@skillprint.co>
2024-08-07 08:52:32 +0530

    updating replay button api

--- task-launcher/src/tasks/shared/helpers/replayAudio.ts
+++ task-launcher/src/tasks/shared/helpers/replayAudio.ts
@@ -1,15 +1,8 @@
-//@ts-ignore
-import { jsPsych } from "../../taskSetup";
-//@ts-ignore
-import { mediaAssets } from "../../..";
-//@ts-ignore
-import { camelize } from "@bdelab/roar-utils";
 import { PageAudioHandler } from "./audioHandler";
 import { PageStateHandler } from "./PageStateHandler";
 
 
-export async function setupReplayAudio(audioFile: string, pageStateHandler: PageStateHandler) {
-
+export async function setupReplayAudio(pageStateHandler: PageStateHandler) {
   if (pageStateHandler.replayBtn) {
     pageStateHandler.disableReplayBtn();
     const enableDelayBuffer = 100; //in ms
