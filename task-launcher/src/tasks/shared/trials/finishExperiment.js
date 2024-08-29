7e68b78078fb8dc23c2776894825b42864687780
zwatson2001 <zwatson@vassar.edu>
2024-08-29 13:08:43 -0400

    give finishExperiment.js same behavior as taskFinished.js rather than skipping trials

--- task-launcher/src/tasks/shared/trials/finishExperiment.js
+++ task-launcher/src/tasks/shared/trials/finishExperiment.js
@@ -10,7 +10,20 @@ export function finishExperiment() {
             <div class='lev-row-container instruction'>
                 <h1 class='instructions-title'>${t.taskFinished}</h1>
             </div>
+            <footer>${t.generalFooter}</footer>
+            <button id="exit-button" class="primary" style=margin-top:5%>Exit</button>
         </div>`,
         PageAudioHandler.playAudio(mediaAssets.audio.taskFinished)
     ); 
+
+    window.addEventListener('click', (event) => {
+        const buttonId = event.target.id;
+        if (buttonId === 'exit-button') {
+            document.body.innerHTML = '';
+        }
+    }); 
+
+    document.addEventListener('keydown', () => {
+            document.body.innerHTML = '';
+    });
 }
