06bfdf26fb157cbe0188a2fc75575ca52d82c312
Aritra <aritra@skillprint.co>
2024-08-30 23:17:01 +0530

    updating trog with the new config

--- task-launcher/src/tasks/trog/helpers/config.ts
+++ task-launcher/src/tasks/trog/helpers/config.ts
@@ -1,17 +1,24 @@
 import { camelize } from "../../shared/helpers/camelize";
+//@ts-ignore
+import { prepareChoices } from "../../shared/helpers/prepareChoices";
 import { DEFAULT_LAYOUT_CONFIG } from "../../shared/helpers/config";
 
 export const getLayoutConfig = (stimulus: StimulusType): LayoutConfigType => {
   const defaultConfig: LayoutConfigType = JSON.parse(JSON.stringify(DEFAULT_LAYOUT_CONFIG));
-  defaultConfig.prompt = {
-    enabled: false,
-    aboveStimulus: false,
-  };
+  const { answer, distractors, trialType } = stimulus;
   defaultConfig.isPracticeTrial = stimulus.assessmentStage === 'practice_response';
   defaultConfig.isInstructionTrial = stimulus.trialType === 'instructions';
+  defaultConfig.showStimImage = false;
+  defaultConfig.showStimText = false;
   if (!defaultConfig.isInstructionTrial) {
+    defaultConfig.prompt = {
+      enabled: false,
+      aboveStimulus: false,
+    };
+    defaultConfig.isImageButtonResponse = true;
+    defaultConfig.buttonChoices = prepareChoices(answer, distractors, true, trialType).choices;
     defaultConfig.classOverrides.buttonClassList = ['image-medium'];
-    defaultConfig.classOverrides.buttonContainerClassList = ['lev-response-row-inline', 'grid-2x2'];
+    defaultConfig.classOverrides.buttonContainerClassList = ['lev-response-row-inline', 'grid-2x2'];    
   } else {
     defaultConfig.classOverrides.buttonClassList = ['primary'];
   }
