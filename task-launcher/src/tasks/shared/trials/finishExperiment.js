9b7893a387b05a26891e45cab09d851fef82b601
zwatson2001 <zwatson@vassar.edu>
2024-08-30 11:44:17 -0400

    update class declarations and remove event listeners after html is cleared

--- task-launcher/src/tasks/shared/trials/finishExperiment.js
+++ task-launcher/src/tasks/shared/trials/finishExperiment.js
@@ -5,25 +5,29 @@ import { mediaAssets} from '../../..';
 
 export function finishExperiment() {
     const t = taskStore().translations;
+    const removeDOMElements = (event) => {
+        if (event.type === 'click'){
+            const buttonId = event.target.id;
+            if (buttonId === 'exit-button') {
+                document.body.innerHTML = '';
+                window.removeEventListener('click', removeDOMElements);
+            }
+        } else if (event.type === 'keydown'){
+            document.body.innerHTML = '';
+            window.removeEventListener('keydown', removeDOMElements);
+        }
+    }
+    window.addEventListener('click', removeDOMElements);S
+    window.addEventListener('keydown', removeDOMElements);
+
     jsPsych.endExperiment(
-        `<div class='instructions-container'>
+        `<div class='lev-stimulus-container'>
             <div class='lev-row-container instruction'>
-                <h1 class='instructions-title'>${t.taskFinished}</h1>
+                <h1>${t.taskFinished}</h1>
             </div>
             <footer>${t.generalFooter}</footer>
             <button id="exit-button" class="primary" style=margin-top:5%>Exit</button>
         </div>`,
         PageAudioHandler.playAudio(mediaAssets.audio.taskFinished)
     ); 
-
-    window.addEventListener('click', (event) => {
-        const buttonId = event.target.id;
-        if (buttonId === 'exit-button') {
-            document.body.innerHTML = '';
-        }
-    }); 
-
-    document.addEventListener('keydown', () => {
-            document.body.innerHTML = '';
-    });
 }
