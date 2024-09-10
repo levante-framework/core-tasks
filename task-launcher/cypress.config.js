e654d8bb3c9e83c66489a9aabd440fbe5b6bf667
zwatson2001 <zwatson@vassar.edu>
2024-09-10 11:57:16 -0400

    remove fullscreen start from cypress config file

--- task-launcher/cypress.config.js
+++ task-launcher/cypress.config.js
@@ -3,15 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        console.log(launchOptions.args)
-        if (browser.family === 'chromium') {
-          launchOptions.args.push('--full-screen')
-        }
-        //console.log(launchOptions.args)
-      
-        return launchOptions
-      })
+      // implement node event listeners here
     },
   },
 });
