ed7c4fca8946e82edd2e40474f272c24e04d55ae
zwatson2001 <zwatson@vassar.edu>
2024-09-13 12:37:03 -0400

    prevent replay button from being disabled when no audio

--- task-launcher/src/tasks/shared/helpers/replayAudio.ts
+++ task-launcher/src/tasks/shared/helpers/replayAudio.ts
@@ -2,15 +2,17 @@ import { PageAudioHandler } from "./audioHandler";
 import { PageStateHandler } from "./PageStateHandler";
 
 
-export async function setupReplayAudio(pageStateHandler: PageStateHandler) {
+export async function setupReplayAudio(pageStateHandler: PageStateHandler, noAudio: boolean = false) {
   if (pageStateHandler.replayBtn) {
-    pageStateHandler.disableReplayBtn();
-    const enableDelayBuffer = 100; //in ms
-    const totalStimulusDurationMs = await pageStateHandler.getStimulusDurationMs(); //in ms
-    const totalDelay = totalStimulusDurationMs + enableDelayBuffer;
-    setTimeout(() => {
-      pageStateHandler.enableReplayBtn();
-    }, totalDelay);
+    if (!noAudio){
+      pageStateHandler.disableReplayBtn();
+      const enableDelayBuffer = 100; //in ms
+      const totalStimulusDurationMs = await pageStateHandler.getStimulusDurationMs(); //in ms
+      const totalDelay = totalStimulusDurationMs + enableDelayBuffer;
+      setTimeout(() => {
+        pageStateHandler.enableReplayBtn();
+      }, totalDelay);
+    }
 
     const onAudioEnd = () => {
       pageStateHandler.enableReplayBtn();
