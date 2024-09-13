fe200f3f7b0b94a29481ad01e4d2038549cb7d4a
zwatson2001 <zwatson@vassar.edu>
2024-09-13 10:57:41 -0400

    change to image-medium class

--- task-launcher/cypress/e2e/helpers.cy.js
+++ task-launcher/cypress/e2e/helpers.cy.js
@@ -11,13 +11,13 @@ function clickThroughInstructions(){
   }
 
 // clicks first image option until game is over
-function selectImages(){
+function selectMediumImages(){
     // first wait for fixation cross to go away 
     cy.wait(500);
     cy.get('.jspsych-content').then((content) => {
       if (content.find('.jspsych-audio-multi-response-button').length === 4){ // should be 4 buttons on screen
-        cy.get('.image').eq(0).click();
-        selectImages();  
+        cy.get('.image-medium').eq(0).click();
+        selectMediumImages();  
       } else {
         cy.contains('Thank you!').should('exist');
         return; 
@@ -30,5 +30,5 @@ export function testImageAfc(){
     cy.contains('OK', {timeout: 120000}).should('be.visible'); 
     cy.contains('OK').realClick(); // real click mimics user gesture so that fullscreen can start
     clickThroughInstructions(); 
-    selectImages();
+    selectMediumImages();
 }
