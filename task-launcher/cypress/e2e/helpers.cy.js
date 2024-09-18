28a95e089062273adcadce26639aa25a61bbda9e
zwatson2001 <zwatson@vassar.edu>
2024-09-18 15:14:30 -0400

    select only correct answers in vocab using Cypress flag

--- task-launcher/cypress/e2e/helpers.cy.js
+++ task-launcher/cypress/e2e/helpers.cy.js
@@ -11,13 +11,22 @@ function clickThroughInstructions(){
   }
 
 // clicks first image option until game is over
-function selectMediumImages(){
+function selectMediumImages(correct){
     // first wait for fixation cross to go away 
     cy.wait(500);
     cy.get('.jspsych-content').then((content) => {
       if (content.find('.jspsych-audio-multi-response-button').length === 4){ // should be 4 buttons on screen
-        cy.get('.image-medium').eq(0).click();
-        selectMediumImages();  
+        if (correct){ 
+          cy.get('.image-medium').each((button) => {
+            const image = button.find('img')[0]; 
+            if ((image.alt).includes('PICK-ME!')){
+              image.click(); 
+            } 
+          })
+        } else {
+          cy.get('.image-medium').eq(0).click();
+        }
+        selectMediumImages(correct);  
       } else {
         cy.contains('Thank you!').should('exist');
         return; 
@@ -25,10 +34,10 @@ function selectMediumImages(){
     });
   }
 
-export function testImageAfc(){
+export function testImageAfc(correct){
     // wait for OK button to be visible
     cy.contains('OK', {timeout: 120000}).should('be.visible'); 
     cy.contains('OK').realClick(); // real click mimics user gesture so that fullscreen can start
     clickThroughInstructions(); 
-    selectMediumImages();
+    selectMediumImages(correct);
 }
