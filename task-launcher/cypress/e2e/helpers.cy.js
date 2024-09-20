7eccf00d579cc5aaa6a644e9b739cd84de1ad7bc
zwatson2001 <zwatson@vassar.edu>
2024-09-20 10:54:38 -0400

    finalize mental rotation test

--- task-launcher/cypress/e2e/helpers.cy.js
+++ task-launcher/cypress/e2e/helpers.cy.js
@@ -10,29 +10,45 @@ function clickThroughInstructions(){
     });
   }
 
+function handlePracticeButtons(){
+  cy.wait(500);
+  cy.get('.jspsych-content').then((content) => {
+    const practiceButtons = content.find('.practice-btn');
+    if (practiceButtons.length > 0){
+      cy.get('.practice-btn').each((button) => {
+        button.click(); 
+      })
+      handlePracticeButtons();
+    } else {
+      return 
+    }
+  })
+}
+
 // clicks first image option until game is over
 function selectImages(correct, imgClass, numberOfButtons){
-    // first wait for fixation cross to go away 
-    cy.wait(500);
-    cy.get('.jspsych-content').then((content) => {
-      if (content.find('.jspsych-audio-multi-response-button').length === numberOfButtons){ 
-        if (correct){ 
-          cy.get(imgClass).each((button) => {
-            const image = button.find('img')[0]; 
-            if ((image.alt).includes('correct')){
-              button.click(); 
-            } 
-          })
-        } else {
-          cy.get(imgClass).eq(0).click();
-        }
-        selectImages(correct, imgClass, numberOfButtons);  
+  handlePracticeButtons(); 
+  cy.get('.lev-stimulus-container', {timeout: 10000}).should('exist'); 
+  // wait for fixation cross to go away 
+  cy.get('.jspsych-content').then((content) => {
+    if (content.find('.jspsych-audio-multi-response-button').length === numberOfButtons){ 
+      if (correct){ 
+        cy.get(imgClass).each((button) => {
+          const image = button.find('img')[0]; 
+          if ((image.alt).includes('correct')){
+            button.click(); 
+          } 
+        })
       } else {
-        cy.contains('Thank you!').should('exist');
-        return; 
+        cy.get(imgClass).eq(0).click();
       }
-    });
-  }
+      selectImages(correct, imgClass, numberOfButtons);  
+    } else {
+      cy.contains('Thank you!').should('exist');
+      return; 
+    }
+  });
+}
 
 export function testImageAfc(correct, imgClass, numberOfButtons){
     // wait for OK button to be visible
