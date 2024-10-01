6eb4767512a737f905ee3a20c4eef7d86aede9a8
zwatson2001 <zwatson@vassar.edu>
2024-10-01 11:24:08 -0400

    start math (works up to slider trials)

--- task-launcher/cypress/e2e/matrix_reasoning.cy.js
+++ task-launcher/cypress/e2e/matrix_reasoning.cy.js
@@ -1,10 +1,10 @@
-import {testImageAfc} from './helpers.cy.js'
+import {testAfc} from './helpers.cy.js'
 
 const matrix_reasoning_url = 'http://localhost:8080/?task=matrix-reasoning'
 
 describe('test matrix reasoning', () => {
   it('visits matrix reasoning and plays game', () => {
     cy.visit(matrix_reasoning_url);
-    testImageAfc(true, '.image');
+    testAfc();
   })
 })
