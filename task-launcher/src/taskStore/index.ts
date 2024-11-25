1dbdfea8101590a3789ddecbd86579252a948f8f
emily-ejag <emily.artegar@gmail.com>
2024-11-25 10:32:45 -0800

    adding numItemsPerStory url param

--- task-launcher/src/taskStore/index.ts
+++ task-launcher/src/taskStore/index.ts
@@ -101,6 +101,7 @@ export const setTaskStore = (config: TaskStoreDataType) => {
     stimulusPosition: 0,
     isCorrect: false,
     inferenceNumStories: config.inferenceNumStories,
+    numItemsPerStory: config.numItemsPerStory,
   });
 };
 
