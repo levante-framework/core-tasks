fa36493a6cf29156fab21267674950058bdb4368
Aritra <aritra@skillprint.co>
2024-12-11 20:00:56 +0530

    refactor a bunch of helper functions

--- task-launcher/src/tasks/shared/helpers/getStimulus.ts
+++ task-launcher/src/tasks/shared/helpers/getStimulus.ts
@@ -1,15 +1,14 @@
-import store from 'store2';
-import { cat, jsPsych } from '../../taskSetup';
 import _isEqual from 'lodash/isEqual';
 import { mediaAssets } from '../../..';
-import { camelize } from '@bdelab/roar-utils';
+import { camelize } from './camelize';
 import { taskStore } from '../../../taskStore';
+import { cat, jsPsych } from '../../taskSetup';
 
 // This function reads the corpus, calls the adaptive algorithm to select
 // the next item, stores it in a session variable, and removes it from the corpus
 // corpusType is the name of the subTask's corpus within corpusLetterAll[]
 
-export const getStimulus = (corpusType) => {
+export const getStimulus = (corpusType: string) => {
   let corpus, itemSuggestion;
 
   corpus = taskStore().corpora;
@@ -30,23 +29,4 @@ export const getStimulus = (corpusType) => {
   // update the corpus with the remaining unused items
   corpus[corpusType] = itemSuggestion.remainingStimuli;
   taskStore('corpora', corpus);
-
-  // --------------------------------------------------------
-
-  // Testing - Slider AFC trials
-  // const afcStim = corpus[corpusType].find(stim => stim.trialType === 'Number Line 4afc')
-  // taskStore('nextStimulus', afcStim);
-
-  // Testing - 0-1 range slider trials
-  // const sliderStim = corpus[corpusType].filter(stim => _isEqual(stim.item, [0,1]))
-  // console.log(sliderStim)
-  // taskStore('nextStimulus', sliderStim[0]);
-
-  // Testing - Number Comparison (2afc)
-  // itemSuggestion = corpus[corpusType].find(stim => stim.trialType === 'Number Comparison')
-  // taskStore('nextStimulus', itemSuggestion);
-
-  // // Testing - same-different-selection
-  // itemSuggestion = corpus[corpusType].find(stim => stim.trialType.includes('something-same-1'))
-  // taskStore('nextStimulus', itemSuggestion);
 };
