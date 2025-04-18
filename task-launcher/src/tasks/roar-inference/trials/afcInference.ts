1c1439ad0234a6234ad0d1781e49183cc29a545d
emily-ejag <emily.artegar@gmail.com>
2025-04-18 14:51:59 -0700

    adding a div container

--- task-launcher/src/tasks/roar-inference/trials/afcInference.ts
+++ task-launcher/src/tasks/roar-inference/trials/afcInference.ts
@@ -196,6 +196,11 @@ function doOnLoad(layoutConfigMap: Record<string, LayoutConfigTypeInference>) {
   if (!isPracticeTrial && !isInstructionTrial) {
     trialsOfCurrentType += 1;
   }
+  const jsPsychContent = document.querySelector('.jspsych-content') as HTMLElement;
+
+if (itemLayoutConfig?.classOverrides.stimulusContainerClassList.includes('inference-scroll')) {
+  jsPsychContent?.classList.add('inference-scroll');
+}
 
   if (stim.trialType !== 'instructions') {
     const buttonContainer = document.getElementById('jspsych-html-multi-response-btngroup') as HTMLDivElement;
@@ -311,6 +316,8 @@ function doOnFinish(data: any, task: string, layoutConfigMap: Record<string, Lay
   } else if (taskStore().numIncorrect >= taskStore().maxIncorrect) {
     finishExperiment();
   }
+  document.querySelector('.jspsych-content')?.classList.remove('inference-scroll');
+
 }
 
 export interface AfcStimulusInput {
