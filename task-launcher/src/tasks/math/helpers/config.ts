b912dfba52d47b42ad04f50d82c06810f3693e30
zwatson2001 <zwatson@vassar.edu>
2025-04-25 16:35:48 -0700

    update styling for slider image container

--- task-launcher/src/tasks/math/helpers/config.ts
+++ task-launcher/src/tasks/math/helpers/config.ts
@@ -49,7 +49,9 @@ export const getLayoutConfig = (
     }
   } else {
     defaultConfig.classOverrides.buttonClassList = ['primary'];
-    defaultConfig.classOverrides.stimulusContainerClassList = ['lev-row-container'];
+    stimulus.trialType === "instructions" ? 
+      defaultConfig.classOverrides.stimulusContainerClassList = ['lev-instructions-container'] :
+      defaultConfig.classOverrides.stimulusContainerClassList = ['lev-row-container']
   }
 
   const messages = validateLayoutConfig(defaultConfig, mediaAssets, translations, stimulus);
