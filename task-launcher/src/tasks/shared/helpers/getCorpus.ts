582eeb8ed534c282748c7e86af08b37636cac289
zwatson2001 <zwatson@vassar.edu>
2025-04-30 13:53:17 -0700

    add option to run adult reasoning as an 8-item cat

--- task-launcher/src/tasks/shared/helpers/getCorpus.ts
+++ task-launcher/src/tasks/shared/helpers/getCorpus.ts
@@ -121,7 +121,14 @@ const transformCSV = (
       audioFile: row.audio_file,
       // difficulty must be undefined for non-instruction/practice trials to avoid running cat
       difficulty:
-        taskStore().runCat || row.trial_type === 'instructions' || row.assessment_stage === 'practice_response'
+        taskStore().runCat || 
+        (
+          (
+            row.trial_type === 'instructions' || 
+            row.assessment_stage === 'practice_response'
+          ) && 
+          taskStore().heavyInstructions
+        )
           ? parseFloat(row.d || row.difficulty)
           : NaN,
       trialNumber: row.trial_num,
