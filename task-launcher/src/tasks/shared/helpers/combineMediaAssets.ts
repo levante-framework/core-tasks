64d75656c52c1a66cfc55d7fe7d05889374b6d30
zwatson2001 <zwatson@vassar.edu>
2025-05-07 16:01:21 -0700

    combine task-specific media assets with shared assets

---
+++ task-launcher/src/tasks/shared/helpers/combineMediaAssets.ts
@@ -0,0 +1,22 @@
+export function combineMediaAssets(
+    mediaAssets: MediaAssetsType, 
+    sharedMediaAssets: MediaAssetsType
+) {
+    mediaAssets.audio = combineMediaType(mediaAssets, sharedMediaAssets, 'audio');
+    mediaAssets.images = combineMediaType(mediaAssets, sharedMediaAssets, 'images');
+    mediaAssets.video = combineMediaType(mediaAssets, sharedMediaAssets, 'video');
+
+    return mediaAssets; 
+}
+
+function combineMediaType(
+    mediaAssets: MediaAssetsType, 
+    sharedMediaAssets: MediaAssetsType, 
+    mediaType: 'audio' | 'images' | 'video'
+) {
+    Object.keys(sharedMediaAssets[mediaType]).forEach((key) => {
+        mediaAssets[mediaType][key] = sharedMediaAssets[mediaType][key]; 
+    });
+
+    return mediaAssets[mediaType];
+}
