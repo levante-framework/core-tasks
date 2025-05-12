452e5cfa3a6d8e367c95a9d17822b2e66c0e187a
zwatson2001 <zwatson@vassar.edu>
2025-05-12 11:48:56 -0700

    create config for what's on this card trials

--- task-launcher/src/tasks/math/helpers/config.ts
+++ task-launcher/src/tasks/math/helpers/config.ts
@@ -23,7 +23,7 @@ export const getLayoutConfig = (
   const stimItem = convertItemToString(stimulus.item);
   defaultConfig.isPracticeTrial = stimulus.assessmentStage === 'practice_response';
   defaultConfig.isInstructionTrial = stimulus.trialType === 'instructions';
-  defaultConfig.showStimImage = stimulus.trialType === 'instructions';
+  defaultConfig.showStimImage = stimulus.trialType === 'instructions' || stimulus.trialType === 'Counting';
   defaultConfig.stimText = {
     value: stimItem,
     displayValue: undefined,
@@ -31,18 +31,21 @@ export const getLayoutConfig = (
   defaultConfig.inCorrectTrialConfig.onIncorrectTrial = 'skip';
   if (!defaultConfig.isInstructionTrial) {
     const mappedDistractors = mapDistractorsToString(distractors);
-    const prepChoices = prepareChoices(answer.toString(), mappedDistractors, 'yes', trialType); 
+    const prepChoices = trialType === "Counting" ? 
+      prepareChoices(answer.toString(), mappedDistractors, 'no', trialType) :
+      prepareChoices(answer.toString(), mappedDistractors, 'yes', trialType); 
     defaultConfig.prompt.enabled = false;
     defaultConfig.isImageButtonResponse = (trialType === "Non-symbolic Number Identification");
     defaultConfig.classOverrides.buttonClassList = 
-      trialType === "Non-symbolic Number Identification" ? ['image-medium'] : ['secondary'];
+      trialType === "Non-symbolic Number Identification" ? ['image-medium'] : 
+      (trialType === "Counting" ? ['primary'] : ['secondary']); 
     defaultConfig.response = {
       target: prepChoices.target,
       displayValues: prepChoices.choices,
       values: prepChoices.originalChoices,
       targetIndex: prepChoices.correctResponseIdx,
     };
-    if (!['Number Identification', 'Number Comparison'].includes(stimulus.trialType)) {
+    if (!['Number Identification', 'Number Comparison', 'Counting'].includes(stimulus.trialType)) {
       defaultConfig.stimText = {
         value: stimItem,
         displayValue: stimulus.trialType === 'Fraction' ? fractionToMathML(stimItem) : stimItem,
