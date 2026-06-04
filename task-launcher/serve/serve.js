abb85c4366f2d1285c81afdf13571e7822b1cfbf
Rodrigo Olivares <rolivares6793@gmail.com>
2026-06-04 10:36:17 -0300

    soft-delete assessmentUid

--- task-launcher/serve/serve.js
+++ task-launcher/serve/serve.js
@@ -58,7 +58,6 @@ const storeItemId = stringToBoolean(urlParams.get('storeItemId'), false);
 const cat = stringToBoolean(urlParams.get('cat'), false);
 const heavyInstructions = stringToBoolean(urlParams.get('heavyInstructions'), false);
 
-
 const emulatorConfig = EMULATORS ? firebaseJSON.emulators : undefined;
 // if running in demo mode, no data will be saved to Firestore
 const demoMode = DEMO;
@@ -69,7 +68,7 @@ async function startWebApp() {
   onAuthStateChanged(appKit.auth, (user) => {
     if (user) {
       const userInfo = {
-        assessmentUid: user.uid,
+        // assessmentUid: user.uid,
         userMetadata: {},
       };
 
