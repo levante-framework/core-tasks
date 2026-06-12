4182fb95365c4aae43e991be02c57e800eca8124
Zach Watson <zdwatson2001@gmail.com>
2026-06-12 14:11:08 -0700

    use individual survey text keys

--- task-launcher/src/tasks/shared/helpers/childSurveyResponses.ts
+++ task-launcher/src/tasks/shared/helpers/childSurveyResponses.ts
@@ -2,7 +2,12 @@ import { taskStore } from '../../../taskStore';
 
 export function getChildSurveyResponses() {
   const t = taskStore().translations;
-  const responses = t.childSurveyRespoonses || t.childSurveyResponses;
+  const responses = [
+    t.childSurveyResponse1, 
+    t.childSurveyResponse2, 
+    t.childSurveyResponse3, 
+    t.childSurveyResponse4
+  ];
 
-  return responses.split(';');
+  return responses;
 }
