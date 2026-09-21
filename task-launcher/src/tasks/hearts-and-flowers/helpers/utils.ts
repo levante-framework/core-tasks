06ec75163f77a5a2ee1faffcb9ce7a83a578091c
cchang-vassar <79338042+cchang-vassar@users.noreply.github.com>
2026-09-21 14:55:15 -0700

    chore: add type aliases for enums, add types to function signatures

--- task-launcher/src/tasks/hearts-and-flowers/helpers/utils.ts
+++ task-launcher/src/tasks/hearts-and-flowers/helpers/utils.ts
@@ -2,46 +2,52 @@ import { taskStore } from '../../../taskStore';
 import { Logger } from '../../../utils/logger';
 import { getParticipantUtilityButtonsHtml } from '../../shared/helpers';
 
-export const StimulusType = Object.freeze({
+export const StimulusType = {
   Heart: 'heart',
   Flower: 'flower',
-});
+} as const;
+export type StimulusType = (typeof StimulusType)[keyof typeof StimulusType];
 
-// Enum for the side the stimulus is shown on and also for button_choices
-export const StimulusSideType = Object.freeze({
+// Enum for the side the stimulus is shown on and also for button_choices (ResponseSideType)
+export const StimulusSideType = {
   Left: 'left',
   Right: 'right',
-});
+} as const;
+export type StimulusSideType = (typeof StimulusSideType)[keyof typeof StimulusSideType];
 
 export const ResponseSideType = StimulusSideType;
+export type ResponseSideType = (typeof ResponseSideType)[keyof typeof ResponseSideType];
 
 // Enum for the jsPsych keyboard inputs
-export const InputKey = Object.freeze({
+export const InputKey = {
   ArrowLeft: 'arrowleft',
   ArrowRight: 'arrowright',
   NoKeys: 'NO_KEYS', // aka no key input accepted
-  AllKeys: 'ALL_KEYS', // aka any keys is accepted
+  AllKeys: 'ALL_KEYS', // aka any key is accepted
   SpaceBar: ' ',
   Enter: 'enter',
-});
+} as const;
+export type InputKey = (typeof InputKey)[keyof typeof InputKey];
 
-export const AssessmentStageType = Object.freeze({
+export const AssessmentStageType = {
   HeartsPractice: 'practice_response',
   FlowersPractice: 'practice_response',
   HeartsStimulus: 'test_response',
   FlowersStimulus: 'test_response',
   HeartsAndFlowersPractice: 'practice_response',
   HeartsAndFlowersStimulus: 'test_response',
-});
+} as const;
+export type AssessmentStageType = (typeof AssessmentStageType)[keyof typeof AssessmentStageType];
 
-export const CorpusTrialType = Object.freeze({
+export const CorpusTrialType = {
   HeartsPractice: 'hearts',
   FlowersPractice: 'flowers',
   HeartsStimulus: 'hearts',
   FlowersStimulus: 'flowers',
   HeartsAndFlowersPractice: 'hearts and flowers',
   HeartsAndFlowersStimulus: 'hearts and flowers',
-});
+} as const;
+export type CorpusTrialType = (typeof CorpusTrialType)[keyof typeof CorpusTrialType];
 
 // TODO: better Exception/Error handling
 /**
@@ -50,7 +56,7 @@ export const CorpusTrialType = Object.freeze({
  * @param {*} stimulusSideType the side of the stimulus: StimulusSideType.Left or StimulusSideType.Right
  * @returns 0 for left, 1 for right
  */
-export function getCorrectInputSide(stimulusType, stimulusSideType) {
+export function getCorrectInputSide(stimulusType: StimulusType, stimulusSideType: StimulusSideType) {
   if (stimulusType === StimulusType.Heart) {
     if (stimulusSideType === StimulusSideType.Left) {
       return 0;
@@ -80,12 +86,17 @@ export function getCorrectInputSide(stimulusType, stimulusSideType) {
  * @param {*} replayButtonHtmlId if you need to show an audio replay button
  * @returns
  */
-export const getStimulusLayout = (imageSrc, isLeft, promptText = undefined, replayButtonHtmlId = undefined) => {
+export const getStimulusLayout = (
+  imageSrc: string,
+  isLeft: boolean,
+  promptText?: string,
+  replayButtonHtmlId?: string,
+): string => {
   const stimulusClass = isLeft ? 'stimulus-left' : 'stimulus-right';
   const includeReplayButton = replayButtonHtmlId !== undefined;
 
   let template = '<div class="haf-stimulus-holder">';
-  template += getParticipantUtilityButtonsHtml(replayButtonHtmlId, includeReplayButton);
+  template += getParticipantUtilityButtonsHtml(replayButtonHtmlId ?? '', includeReplayButton);
 
   if (promptText) {
     template += `
@@ -108,7 +119,7 @@ export const getStimulusLayout = (imageSrc, isLeft, promptText = undefined, repl
   return template;
 };
 
-export const getInputInstructPrompt = (showButton = false) => {
+export const getInputInstructPrompt = (showButton: boolean = false): string => {
   const inputCapability = taskStore().inputCapability;
 
   if (showButton) {
